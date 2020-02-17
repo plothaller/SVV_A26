0f49aee3789067cdc8f52b9558298197c2850283
gpresamagrina <g.presi94@gmail.com>
2020-02-17 15:15:47 +0100

    [step function]

--- Step_function.py
+++ Step_function.py
@@ -1,18 +1,14 @@
 import numpy as np
 
 
-
 def step_matrix(x, step_list):
 	matrix = np.identity(len(step_list))
 	for i in range(0,len(step_list)):
 		if step_list[i] > x:
-			print("11111")
 			matrix[i] = np.zeros(shape=(len(step_list)))
-	print(matrix)
-	
 	return matrix
 
-step_matrix(5,(1,2,8,5))
+print(step_matrix(5,(1,2,8,5)))
 
 	
 	
