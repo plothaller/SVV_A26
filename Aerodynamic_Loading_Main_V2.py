f32ede1af1974d50cec031f7c3add8694322053f
Max van Huffelen <M.C.vanHuffelen@student.tudelft.nl>
2020-02-23 17:54:18 +0100

    minor improvements and reversing direction of integration will change sign of result

--- Aerodynamic_Loading_Main_V2.py
+++ Aerodynamic_Loading_Main_V2.py
@@ -1,23 +1,9 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Feb 23 16:14:23 2020
-
-@author: Max v. Huffelen
-"""
-
 import numpy as np
 
-
-# =============================================================================
-# Notes:
-# file size is 81 x 41
-# =============================================================================
 
 #global variables C_a and l_a for the chord and length of the aileron, respectively
 C_a = 0.484
 l_a = 1.691
-
-
 
 def locationz(idx):
     #inputs: the index of the element in the matrix
@@ -57,10 +43,18 @@ def findIndex(loc_x, loc_z):
         for index_z in range(0, 82):
             if locationz(index_z + 1) < loc_z <= locationz(index_z):
                 return index_z
+        if loc_z > locationz(0):
+            return 0
+        else:
+            return 81
     def findx(loc_x):
         for index_x in range(0, 42):
             if locationx(index_x) <= loc_x < locationx(index_x + 1):
                 return index_x
+        if loc_x < locationx(0):
+            return 0
+        else:
+            return 41
     return findx(loc_x), findz(loc_z)
 
 
@@ -83,26 +77,32 @@ def integrate(x_0, z_0, x_1, z_1, AeroLoad = AeroLoading):
     
     def integrateComponent(index_x, index_z, x_0 = x_0, z_0 = z_0, x_1 = x_1, z_1 = z_1, AeroLoad = AeroLoad):
         #define p11, p12, p21, p22 where p11 has lowest x, z indices, p12 has higher z index, p21 has higher x index, p22 has higher x and z index
+        
+        #Define the loads at the cornerpoints of the component section
         Q11 = AeroLoad[index_z, index_x]
         Q12 = AeroLoad[index_z+1, index_x]
         Q21 = AeroLoad[index_z, index_x+1]
         Q22 = AeroLoad[index_z+1,index_x+1]
+        #Define the locations of the cornerpoints
         x_lower = locationx(index_x)
         x_upper = locationx(index_x+1)
         z_lower = locationz(index_z)    #this is lower INDEX, not lower value
         z_upper = locationz(index_z+1)
+        #Find the values of aero loading (estimated) at each of the corners of the SUB-AREA WE ARE INTERPOLATING, (not the section of aero data)
         p11 = BilinearInterpolate(Q11, Q21, Q12, Q22, x_lower, z_lower, x_upper, z_upper, x_0, z_0)
         p12 = BilinearInterpolate(Q11, Q21, Q12, Q22, x_lower, z_lower, x_upper, z_upper, x_0, z_1)
         p21 = BilinearInterpolate(Q11, Q21, Q12, Q22, x_lower, z_lower, x_upper, z_upper, x_1, z_0)
         p22 = BilinearInterpolate(Q11, Q21, Q12, Q22, x_lower, z_lower, x_upper, z_upper, x_1, z_1)
+        #integrate twice using the trapezoid rule
         return (x_upper-x_lower)*(z_upper-z_lower)*(p11 + p12 + p21 + p22)/4
     
     totalIntegrated = 0
     
-    for index_x in range(index_x_0, index_x_1):
-        for index_z in range(min(index_z_0, index_z_1), max(index_z_0, index_z_1)):
+    for index_x in range(min(index_x_0, index_x_1), min(max(index_x_1, index_x_0), 40)):
+        for index_z in range(min(index_z_0, index_z_1), min(max(index_z_0, index_z_1), 80)):
+            #add the integrated value of one component to the total. Do this for each component to get the total integrated value
             totalIntegrated += integrateComponent(index_x, index_z)
-    return totalIntegrated
+    return totalIntegrated * np.sign((index_x_1-index_x_0)*(index_z_1 - index_z_0))
 
 
 
